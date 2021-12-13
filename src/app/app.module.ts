dbab170cf46e632dad539f8b5f521155d81c15e4
Antim Antim <khairul.auntim@gmail.com>
2021-12-13 23:05:31 +0100

    Intial update from Windows 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { InsertEmployeeComponent } from './insert-employee/insert-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ViewEmployeeComponent,
+    InsertEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
